88ebc2c04eb5711e79e833404b88d5f678479d88
drewm <drew.m.moulton@gmail.com>
2024-02-29 20:27:57 -0800

    Base images

--- src/app/components/drew.tsx
+++ src/app/components/drew.tsx
@@ -4,7 +4,7 @@ export default function Drew() {
     return (
         <div>
             <div className="w-32 h-32">
-                <Image className="rounded-full aspect-square object-cover shadow-lg" src="/DrewMoultonHeadshot.jpeg" alt="Drew headshot" width={200} height={200}/>
+                <Image className="rounded-full object-cover shadow-lg" src="/DrewMoultonHeadshotCropped.jpeg" alt="Drew headshot" width={200} height={200}/>
             </div>
             <p className="pt-4">Hello, this is Drew!</p>
         </div>

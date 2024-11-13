833db4e6fbe87919e1677bf9035ba5a4594132cb
drewm <drew.m.moulton@gmail.com>
2024-11-12 22:46:28 -0800

    Disable build type checking

--- src/components/fluid/webgl-fluid.tsx
+++ src/components/fluid/webgl-fluid.tsx
@@ -1,21 +1,17 @@
 "use client";
 
-import {useEffect, useRef} from "react";
+import {useCallback} from "react";
 import Fluid from "@/fluid/fluid";
 import {PropsWithClassName} from "@/lib/types/class-name-props";
-import {useRefDimensions} from "@/hooks/useRefDimensions";
 import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 export default function WebglFluid(props: PropsWithClassName) {
     const size = useWindowDimensions();
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-
-    useEffect(() => {
-        if (canvasRef.current) {
-            let fluid = new Fluid(canvasRef.current);
-            fluid.activate();
-        }
-    }, [canvasRef.current]);
+    const canvasRef = useCallback((canvas: HTMLCanvasElement | null) => {
+        if (!canvas) return;
+        let fluid = new Fluid(canvas);
+        fluid.activate();
+    }, []);
 
     return (
         <div className={props.className}>
